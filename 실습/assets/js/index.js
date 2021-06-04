cf038063ad331476e8890edec0f3e8d774f6f7f0
박현우(Park Hyun Woo) <46596758+gusdn3477@users.noreply.github.com>
2021-06-04 16:57:58 +0900

    수정
    하드코딩한 url 파일들은 올리지 않았습니다.

--- 실습/assets/js/index.js
+++ 실습/assets/js/index.js
@@ -1,28 +1,28 @@
 let text = `{"employees" : [
-    {"firstName" : "John", "lastName" : "Doe"},
-    {"firstName" : "Anna", "lastName" : "Smith"},
-    {"firstName" : "Peter", "lastName" : "Jones"},
-    {"firstName" : "Hyunwoo", "lastName" : "Park"},
-    {"firstName" : "Heungmin", "lastName" : "Son"},
-    {"firstName" : "Jisung", "lastName" : "Park"},
-    {"firstName" : "Gildong", "lastName" : "Hong"},
-    {"firstName" : "John", "lastName" : "Doe"},
-    {"firstName" : "Anna", "lastName" : "Smith"},
-    {"firstName" : "Peter", "lastName" : "Jones"},
-    {"firstName" : "Hyunwoo", "lastName" : "Park"},
-    {"firstName" : "Heungmin", "lastName" : "Son"},
-    {"firstName" : "Jisung", "lastName" : "Park"},
-    {"firstName" : "Gildong", "lastName" : "Hong"},
-    {"firstName" : "John", "lastName" : "Doe"},
-    {"firstName" : "Anna", "lastName" : "Smith"},
-    {"firstName" : "Peter", "lastName" : "Jones"},
-    {"firstName" : "Hyunwoo", "lastName" : "Park"},
-    {"firstName" : "Heungmin", "lastName" : "Son"},
-    {"firstName" : "Jisung", "lastName" : "Park"},
-    {"firstName" : "Gildong", "lastName" : "Hong"},
-    {"firstName" : "Tmax", "lastName" : "Cloud"},
-    {"firstName" : "Anna", "lastName" : "Smith"},
-    {"firstName" : "Peter", "lastName" : "Jones"}
+    {"firstName" : "John", "lastName" : "Doe", "url" : "1.html"},
+    {"firstName" : "Anna", "lastName" : "Smith", "url" : "2.html"},
+    {"firstName" : "Peter", "lastName" : "Jones", "url" : "3.html"},
+    {"firstName" : "Hyunwoo", "lastName" : "Park", "url" : "4.html"},
+    {"firstName" : "Heungmin", "lastName" : "Son", "url" : "5.html"},
+    {"firstName" : "Jisung", "lastName" : "Park", "url" : "6.html"},
+    {"firstName" : "Gildong", "lastName" : "Hong", "url" : "7.html"},
+    {"firstName" : "John", "lastName" : "Doe", "url" : "8.html"},
+    {"firstName" : "Anna", "lastName" : "Smith", "url" : "9.html"},
+    {"firstName" : "Peter", "lastName" : "Jones", "url" : "10.html"},
+    {"firstName" : "Hyunwoo", "lastName" : "Park", "url" : "11.html"},
+    {"firstName" : "Heungmin", "lastName" : "Son", "url" : "12.html"},
+    {"firstName" : "Jisung", "lastName" : "Park", "url" : "13.html"},
+    {"firstName" : "Gildong", "lastName" : "Hong", "url" : "14.html"},
+    {"firstName" : "John", "lastName" : "Doe", "url" : "15.html"},
+    {"firstName" : "Anna", "lastName" : "Smith", "url" : "16.html"},
+    {"firstName" : "Peter", "lastName" : "Jones", "url" : "17.html"},
+    {"firstName" : "Hyunwoo", "lastName" : "Park", "url" : "18.html"},
+    {"firstName" : "Heungmin", "lastName" : "Son", "url" : "19.html"},
+    {"firstName" : "Jisung", "lastName" : "Park", "url" : "20.html"},
+    {"firstName" : "Gildong", "lastName" : "Hong", "url" : "21.html"},
+    {"firstName" : "Tmax", "lastName" : "Cloud", "url" : "22.html"},
+    {"firstName" : "Anna", "lastName" : "Smith", "url" : "23.html"},
+    {"firstName" : "Peter", "lastName" : "Jones", "url" : "24.html"}
 ]}`
 
 obj = JSON.parse(text);
@@ -35,19 +35,22 @@ for (let i=0;i<24;i++){
 
 vi += `<div class=box_outer>`
 for(let i = 0; i<obj.employees.length/3; i++){
-    vi += `<div class="box" onclick="alert('이름 : ${obj.employees[i].firstName} ${obj.employees[i].lastName}');">` + obj.employees[i].firstName + ' ' + obj.employees[i].lastName + `</div>`;
+    vi += `<div class="box" ><div class="inner_box_outer">` + obj.employees[i].firstName + ' ' + obj.employees[i].lastName + 
+    `<button class="btn" onclick="location.href = './assets/${obj.employees[i].url}'">더 보기</button></div></div>`;
 }
 vi += `</div>`;
 
 vi += `<div class=box_outer2>`
 for(let i = obj.employees.length/3; i<obj.employees.length/3 * 2; i++){
-    vi += `<div class="box" onclick="alert('이름 : ${obj.employees[i].firstName} ${obj.employees[i].lastName}');">` + obj.employees[i].firstName + ' ' + obj.employees[i].lastName + `</div>`;
+    vi += `<div class="box"><div class="inner_box_outer">` + obj.employees[i].firstName + ' ' + obj.employees[i].lastName + 
+    `<button class="btn" onclick="location.href = './assets/${obj.employees[i].url}'">더 보기</button></div></div>`;
 }
 vi += `</div>`;
 
 vi += `<div class=box_outer3>`
 for(let i = obj.employees.length/3 * 2; i<obj.employees.length; i++){
-    vi += `<div class="box" onclick="alert('이름 : ${obj.employees[i].firstName} ${obj.employees[i].lastName}');">` + obj.employees[i].firstName + ' ' + obj.employees[i].lastName + `</div>`;
+    vi += `<div class="box"><div class="inner_box_outer">` + obj.employees[i].firstName + ' ' + obj.employees[i].lastName +
+    `<button class="btn" onclick="location.href = './assets/${obj.employees[i].url}'">더 보기</button></div></div>`;
 }
 vi += `</div>`;
 
